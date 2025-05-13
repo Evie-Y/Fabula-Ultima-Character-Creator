de0da6a19a9a890806197bbccc927b7da0c9add2
Evie <eyg230000@utdallas.edu>
2025-05-12 23:07:07 -0500

    insert rectangle

--- src/character.py
+++ src/character.py
@@ -1,10 +1,15 @@
 import pygame
 import sys 
 
+def draw_text(text, font, text_color, x, y,screen):
+    img = font.render(text, False, text_color)
+    screen.blit(img, (x, y))
+
 def main():
     pygame.init()
+    pygame.font.init()
 
-    pygame.display.set_caption("Fabula Ultima Character Creator")
+    pygame.display.set_caption("Fabula_Ultima Character Creator")
 
     info = pygame.display.Info()
     s_width = info.current_w
@@ -15,24 +20,14 @@ def main():
     background = (255, 230, 162)
     color_light = (215, 240, 255)
     color_dark = (160, 222, 255)
-    screen.fill((background))
-
-    text_font = pygame.font.SysFont("nirmalatext", 32)
-    text = text_font.render('quit' , True , color)
-    mouse = pygame.mouse.get_pos() 
-    if s_width/2 <= mouse[0] <= s_width/2+140 and s_height/2 <= mouse[1] <= s_height/2+40:  
-        pygame.draw.rect(screen,color_light,[s_width/2,s_height/2,140,40])  
-    else:  
-        pygame.draw.rect(screen,color_dark,[s_width/2,s_height/2,140,40])  
-        screen.blit(text, (s_width/2+50,s_height/2))  
-    while True:  
-        for ev in pygame.event.get():     
-            if ev.type == pygame.QUIT:  
-                pygame.quit()        
-            if ev.type == pygame.MOUSEBUTTONDOWN:  
-                if s_width/2 <= mouse[0] <= s_width/2+140 and s_height/2 <= mouse[1] <= s_height/2+40:  
-                    pygame.quit()
-    pygame.display.flip()
+    running = True
+    while running:
+        screen.fill((background))
+        pygame.draw.rect(screen, color_dark, [590, 315, 80 , 30])
+        text_font = pygame.font.SysFont("nirmalatext", 15)
+        draw_text=("load", text_font, (0,0,0,), 590, 315, screen)
+        pygame.display.flip()
+pygame.quit()
 
 
 if __name__ == "__main__":
